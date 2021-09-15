3cff358223112f46badfd1864e3e59d5d846a1fc
TaeinKim <della.kimko@gmail.com>
2021-09-16 06:33:46 +0900

    Revert "#14 - HTML 파싱"

--- Sanghee/Practice1/Practice1/ViewController.swift
+++ Sanghee/Practice1/Practice1/ViewController.swift
@@ -5,8 +5,6 @@
 //  Created by leeesangheee on 2021/09/16.
 //
 
-import Alamofire
-import Kanna
 import SnapKit
 import UIKit
 
@@ -14,35 +12,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     let tableView = UITableView()
     
-    var page: Int = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         configureTableView()
-        
-        getData()
-    }
-    
-    func getData() {
-        guard let url = URL(string: "https://cse.snu.ac.kr/department-notices?&keys=&page=\(page)") else { return }
-        AF.request(url).responseString { response in
-            guard let html = response.value else { return }
-            do {
-                let doc = try Kanna.HTML(html: html, encoding: .utf8)
-                let docTexts = doc.css("td")
-                for docText in docTexts{
-                    guard let text = docText.text else { return }
-                    let startIdx: String.Index = text.index(text.startIndex, offsetBy: 13)
-                    let endIdx: String.Index = text.index(text.endIndex, offsetBy: -11)
-                    let slicedText = text[startIdx...endIdx]
-                    print(slicedText)
-                }
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
     }
     
     func configureTableView() {
@@ -60,6 +34,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
+
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "noticeCell", for: indexPath)
