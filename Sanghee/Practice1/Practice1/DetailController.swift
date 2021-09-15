5b3f8eaae86ba39b4b8427e95a647de38e3fb63f
sanghee-dev <leeesangheee@gmail.com>
2021-09-16 08:49:31 +0900

    #34 상세 화면 제목, 날짜 추가

--- Sanghee/Practice1/Practice1/DetailController.swift
+++ Sanghee/Practice1/Practice1/DetailController.swift
@@ -5,15 +5,23 @@
 //  Created by leeesangheee on 2021/09/16.
 //
 
+import Alamofire
+import Kanna
+import SnapKit
 import UIKit
+import WebKit
 
 class DetailController: UIViewController {
     
     private var notice: Notice?
     
+    private var labelView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        addLabelView()
     }
     
     init(_ notice: Notice) {
@@ -23,6 +31,36 @@ class DetailController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func addLabelView() {
+        view.addSubview(labelView)
+        labelView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(8)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left).inset(8)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).inset(8)
+        }
+        
+        let titleLabel = UILabel()
+        let timeLabel = UILabel()
+        
+        labelView.addSubview(titleLabel)
+        labelView.addSubview(timeLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+        }
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.left.right.equalToSuperview()
+        }
+        
+        titleLabel.text = notice?.title
+        timeLabel.text = notice?.time
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        timeLabel.font = UIFont.systemFont(ofSize: 14)
+        timeLabel.textColor = .gray
     }
     
 }
