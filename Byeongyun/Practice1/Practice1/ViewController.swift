f969869925c9e9dfeda94ae70fd2a9b86ec38b6a
ITlearning <yo7504@naver.com>
2021-09-16 00:46:29 +0900

    Initial Commit

---
+++ Byeongyun/Practice1/Practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice1
+//
+//  Created by ITlearning on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
