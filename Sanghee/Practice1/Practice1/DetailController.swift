d84674305d5c7bb02296af3079f06b1a254876c0
sanghee-dev <leeesangheee@gmail.com>
2021-09-17 23:14:57 +0900

    첨부파일이 있을 때 웹뷰가 가리지 않도록 업데이트

--- Sanghee/Practice1/Practice1/DetailController.swift
+++ Sanghee/Practice1/Practice1/DetailController.swift
@@ -66,6 +66,7 @@ class DetailController: UIViewController, UITableViewDelegate, UITableViewDataSo
                 }
                 if !self.fileUrlList.isEmpty {
                     self.configureModalView()
+                    self.addBottomSpaceToWebView()
                     self.tableView.reloadData()
                 }
             } catch {
@@ -120,6 +121,12 @@ class DetailController: UIViewController, UITableViewDelegate, UITableViewDataSo
         webView.snp.makeConstraints { make in
             make.top.equalTo(labelView.snp.bottom)
             make.left.right.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    private func addBottomSpaceToWebView() {
+        webView.snp.updateConstraints { make in
+            make.bottom.equalToSuperview().offset(modalHeight * (-1))
         }
     }
     
