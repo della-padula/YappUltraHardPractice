90a1fda1f2595d5cc7af425aa6d831e5cca4a787
JiyunPark <39082599+glossyyoon@users.noreply.github.com>
2021-09-27 20:44:10 +0900

    #74 결과 화면 수정2

--- UltraProject1/UltraProject1/ViewControllers/ResultViewController.swift
+++ UltraProject1/UltraProject1/ViewControllers/ResultViewController.swift
@@ -10,72 +10,94 @@ import SnapKit
 import UIKit
 
 class ResultViewController: UIViewController {
-    let resultTitleLabel = UILabel()
-    let resultScoreLabel = UILabel()
-    let try1CountLabel = UILabel()
-    let try2CountLabel = UILabel()
-    let failCountLabel = UILabel()
-    let againButton = UIButton()
+    //MARK: - 라벨 선언
+    let resultTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "게임 결과"
+        label.font = UIFont.systemFont(ofSize: 30)
+        return label
+    }()
+    
+    let resultScoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "총 10회 성공"
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        return label
+    }()
+    
+    let firstTryCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1회 재시도: 7회"
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    let secondTryCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2회 재시도: 3회"
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    let failCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "실패(오답): 5회"
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    //MARK: - 버튼 선언
+    let againButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 1.00, green: 0.70, blue: 0.27, alpha: 1.00)
+        button.setTitle("다시 하기", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         view.backgroundColor = .white
         setLabelUI()
         setButtonUI()
     }
+    
     func setLabelUI(){
         view.addSubview(resultTitleLabel)
         view.addSubview(resultScoreLabel)
-        view.addSubview(try1CountLabel)
-        view.addSubview(try2CountLabel)
+        view.addSubview(firstTryCountLabel)
+        view.addSubview(secondTryCountLabel)
         view.addSubview(failCountLabel)
         
-        resultTitleLabel.text = "게임 결과"
-        resultTitleLabel.font = UIFont.systemFont(ofSize: 30)
-        
-        resultScoreLabel.text = "총 10회 성공"
-        resultScoreLabel.font = UIFont.boldSystemFont(ofSize: 50)
-        
-        try1CountLabel.text = "1회 재시도: 7회"
-        try1CountLabel.font = UIFont.systemFont(ofSize: 20)
-        
-        try2CountLabel.text = "2회 재시도: 3회"
-        try2CountLabel.font = UIFont.systemFont(ofSize: 20)
-        
-        failCountLabel.text = "실패(오답): 5회"
-        failCountLabel.font = UIFont.systemFont(ofSize: 20)
-        
-        resultTitleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(150)
-            make.centerX.equalToSuperview()
+        resultTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(150)
+            $0.centerX.equalToSuperview()
         }
-        resultScoreLabel.snp.makeConstraints { make in
-            make.top.equalTo(resultTitleLabel).offset(100)
-            make.centerX.equalToSuperview()
+        resultScoreLabel.snp.makeConstraints {
+            $0.top.equalTo(resultTitleLabel).offset(100)
+            $0.centerX.equalToSuperview()
         }
-        try1CountLabel.snp.makeConstraints { make in
-            make.top.equalTo(resultScoreLabel).offset(200)
-            make.centerX.equalToSuperview()
+        firstTryCountLabel.snp.makeConstraints {
+            $0.top.equalTo(resultScoreLabel).offset(200)
+            $0.centerX.equalToSuperview()
         }
-        try2CountLabel.snp.makeConstraints { make in
-            make.top.equalTo(try1CountLabel).offset(50)
-            make.centerX.equalToSuperview()
+        secondTryCountLabel.snp.makeConstraints {
+            $0.top.equalTo(firstTryCountLabel).offset(50)
+            $0.centerX.equalToSuperview()
         }
-        failCountLabel.snp.makeConstraints { make in
-            make.top.equalTo(try2CountLabel).offset(50)
-            make.centerX.equalToSuperview()
+        failCountLabel.snp.makeConstraints {
+            $0.top.equalTo(secondTryCountLabel).offset(50)
+            $0.centerX.equalToSuperview()
         }
     }
+    
     func setButtonUI(){
         view.addSubview(againButton)
-        
-        againButton.backgroundColor = UIColor(red: 1.00, green: 0.70, blue: 0.27, alpha: 1.00)
-        againButton.setTitle("다시 하기", for: .normal)
-        againButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
         againButton.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-150)
             make.centerX.equalToSuperview()
         }
     }
+    
     @objc
     func buttonTapped() {
         print(1)
