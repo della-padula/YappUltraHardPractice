d135f04ae41a6c71566c130bd1f9397ad5ee5b52
JiyunPark <39082599+glossyyoon@users.noreply.github.com>
2021-09-27 22:14:16 +0900

    #77 - 결과 화면 resizing, Appdelegate 수정

--- UltraProject1/UltraProject1/AppDelegate.swift
+++ UltraProject1/UltraProject1/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = ResultViewController()
+        window?.rootViewController = LoginViewController()
         window?.makeKeyAndVisible()
         return true
     }
