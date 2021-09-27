1621865308f3aa628e69556b45ea4ebeb1ae7f06
sanghee-dev <leeesangheee@gmail.com>
2021-09-27 18:26:17 +0900

    #70 기록 화면 생성

---
+++ UltraProject1/UltraProject1/ViewControllers/RecordViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RecordViewController.swift
+//  UltraProject1
+//
+//  Created by leeesangheee on 2021/09/27.
+//
+
+import SnapKit
+import UIKit
+
+class RecordViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "RecordCell")
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecordCell", for: indexPath)
+        return cell
+    }
+}
