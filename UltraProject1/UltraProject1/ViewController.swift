cea3797001c69248a55bba6016f236ce88bde7a0
sanghee <61302874+sanghee-dev@users.noreply.github.com>
2021-09-27 20:53:30 +0900

    Revert "#66, #69, #70 로그인, 메인, 기록 화면 생성"

---
+++ UltraProject1/UltraProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UltraProject1
+//
+//  Created by denny on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
