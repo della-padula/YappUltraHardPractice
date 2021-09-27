9d93f7e48fa57326a7892c14ab5c6dd48c656bbf
sanghee-dev <leeesangheee@gmail.com>
2021-09-27 17:18:03 +0900

    #65 초기 설정

---
+++ UltraProject1/UltraProject1/ViewControllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  UltraProject1
+//
+//  Created by leeesangheee on 2021/09/27.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
