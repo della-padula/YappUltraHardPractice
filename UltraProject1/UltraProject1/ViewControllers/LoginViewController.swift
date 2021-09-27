593b83043b58baa89ce6cf6925f8d8c9b9f94df0
sanghee-dev <leeesangheee@gmail.com>
2021-09-27 17:45:36 +0900

    #66 로그인 화면 생성

--- UltraProject1/UltraProject1/ViewControllers/LoginViewController.swift
+++ UltraProject1/UltraProject1/ViewControllers/LoginViewController.swift
@@ -5,11 +5,37 @@
 //  Created by leeesangheee on 2021/09/27.
 //
 
+import SnapKit
 import UIKit
 
 class LoginViewController: UIViewController {
+    private let loginButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        configureLoginButton()
+    }
+    
+    private func configureLoginButton() {
+        view.addSubview(loginButton)
+        
+        loginButton.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(24)
+            make.bottom.equalToSuperview().inset(160)
+            make.height.equalTo(60)
+        }
+        
+        loginButton.setTitle("카카오톡 로그인", for: .normal)
+        loginButton.setTitleColor(.black, for: .normal)
+        loginButton.backgroundColor = .kakaoYellow
+        loginButton.layer.cornerRadius = 16
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped(_:)), for: .touchUpInside)
+    }
+    
+    @objc
+    private func loginButtonTapped(_ sender: UIButton) {
     }
 }
