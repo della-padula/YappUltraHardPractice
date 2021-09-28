481c0373d73a9a43bbad67e0c90a7d07bae9f8c7
sanghee-dev <leeesangheee@gmail.com>
2021-09-29 03:09:36 +0900

    기록셀 UI 수정

--- UltraProject1/UltraProject1/ViewControllers/RecordViewController.swift
+++ UltraProject1/UltraProject1/ViewControllers/RecordViewController.swift
@@ -63,6 +63,6 @@ extension RecordViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        return 64
     }
 }
