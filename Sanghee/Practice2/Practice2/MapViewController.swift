1e28790081993edf2eb68cee3175895d63ce7ce2
sanghee-dev <leeesangheee@gmail.com>
2021-09-30 19:59:35 +0900

    프로젝트 설정

--- Sanghee/Practice2/Practice2/MapViewController.swift
+++ Sanghee/Practice2/Practice2/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Practice2
 //
 //  Created by leeesangheee on 2021/09/30.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
-
