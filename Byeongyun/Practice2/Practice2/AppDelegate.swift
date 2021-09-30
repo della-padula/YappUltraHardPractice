07b26f8daa9cff13937d3013b41496b3265bd8d0
ITlearning <yo7504@naver.com>
2021-09-30 20:54:31 +0900

    #127 - 기본 세팅

---
+++ Byeongyun/Practice2/Practice2/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Practice2
+//
+//  Created by ITlearning on 2021/09/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
