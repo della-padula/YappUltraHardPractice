71f5e024c83118f8db8bdcd20bac97ecfb22e0db
sanghee-dev <leeesangheee@gmail.com>
2021-10-02 16:24:09 +0900

    Coordinate 비교 함수 수정

--- Sanghee/Practice2/Practice2/Extension/CLLocationCoordinate2D+Equatable.swift
+++ Sanghee/Practice2/Practice2/Extension/CLLocationCoordinate2D+Equatable.swift
@@ -7,8 +7,8 @@
 
 import CoreLocation
 
-extension CLLocationCoordinate2D: Equatable {}
-
-public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
-    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
 }
