022e89457bde66948992d0ebb6f2c0adaf3dd6cd
ITlearning <yo7504@naver.com>
2021-10-03 00:05:44 +0900

    #146 - 스토리보드 제거, AppDelegate 세팅

---
+++ Byeongyun/Practice3/Practice3/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Practice3
+//
+//  Created by ITlearning on 2021/10/02.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+
+
+}
+
---
+++ Byeongyun/Practice3/Practice3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice3
+//
+//  Created by ITlearning on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
