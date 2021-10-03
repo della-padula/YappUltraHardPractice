b3741111f17d98046e4028919f941c2a6fb682e3
sanghee-dev <leeesangheee@gmail.com>
2021-10-03 22:44:51 +0900

    #143 메인뷰 프로필 이미지 추가

--- Sanghee/Practice3/Practice3/View/MainViewController.swift
+++ Sanghee/Practice3/Practice3/View/MainViewController.swift
@@ -24,16 +24,22 @@ class MainViewController: UIViewController {
         return label
     }()
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "person.crop.circle"))
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        configureLabels()
+        configureHeader()
     }
     
-    private func configureLabels() {
+    private func configureHeader() {
         view.addSubview(timeLabel)
         view.addSubview(titleLabel)
+        view.addSubview(profileImageView)
         
         timeLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(60)
@@ -42,6 +48,11 @@ class MainViewController: UIViewController {
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(timeLabel.snp.bottom).offset(4)
             $0.left.equalToSuperview().inset(16)
+        }
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.top)
+            $0.right.equalToSuperview().inset(16)
+            $0.width.height.equalTo(36)
         }
     }
 }
