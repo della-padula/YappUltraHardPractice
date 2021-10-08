e7ea022e7249403f43be4d42a1f0c4aab13278cb
sanghee-dev <leeesangheee@gmail.com>
2021-10-08 21:54:21 +0900

    #155 프로젝트 생성

---
+++ Sanghee/Practice4/Practice4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice4
+//
+//  Created by leeesangheee on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
