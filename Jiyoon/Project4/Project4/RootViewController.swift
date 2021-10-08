b1c396a472c19451479a678fe006167d954626e8
JiyunPark <39082599+glossyyoon@users.noreply.github.com>
2021-10-08 23:20:53 +0900

    #156 - 프로젝트 세팅, pod 설치

---
+++ Jiyoon/Project4/Project4/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by 박지윤 on 2021/10/08.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
