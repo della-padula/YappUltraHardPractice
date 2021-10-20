1c4e3e3ca13b7c24a613038100afcc6c1937adb6
ITlearning <yo7504@naver.com>
2021-10-21 02:15:12 +0900

    #193 - 영상 실행, ActivityIndicator 활성화

--- Byeongyun/Practice7/Practice7/Service/MainViewService.swift
+++ Byeongyun/Practice7/Practice7/Service/MainViewService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class MainViewService {
     private static let model = [
-        VideoContent(videoImage: "image01", channelImage: "image02", videoMainLabel: "[키티빌런즈] 루루 끼이이아야야아오옹!!!", profileLabel: "크집사·조회수 25만회·3일전"),
+        VideoContent(videoImage: "image03", channelImage: "image04", videoMainLabel: "로이킴 (Roy Kim) - 살아가는 거야 (Linger On) MV", profileLabel: "Stone Music Entertainment·조회수 151만회·1년 전"),
         VideoContent(videoImage: "image01", channelImage: "image02", videoMainLabel: "[키티빌런즈] 루루 끼이이아야야아오옹!!!", profileLabel: "크집사·조회수 25만회·3일전"),
         VideoContent(videoImage: "image01", channelImage: "image02", videoMainLabel: "[키티빌런즈] 루루 끼이이아야야아오옹!!!", profileLabel: "크집사·조회수 25만회·3일전"),
         VideoContent(videoImage: "image01", channelImage: "image02", videoMainLabel: "[키티빌런즈] 루루 끼이이아야야아오옹!!!", profileLabel: "크집사·조회수 25만회·3일전"),
--- Byeongyun/Practice7/Practice7/View/VideoViewController/VideoViewController.swift
+++ Byeongyun/Practice7/Practice7/View/VideoViewController/VideoViewController.swift
@@ -7,8 +7,18 @@
 
 import UIKit
 import SnapKit
+import AVFoundation
 
 class VideoViewController: UIViewController{
+
+    private let urlString = "https://firebasestorage.googleapis.com/v0/b/writing-3f2f0.appspot.com/o/%E1%84%85%E1%85%A9%E1%84%8B%E1%85%B5%E1%84%8F%E1%85%B5%E1%86%B7(Roy%20Kim)%20-%20%E1%84%89%E1%85%A1%E1%86%AF%E1%84%8B%E1%85%A1%E1%84%80%E1%85%A1%E1%84%82%E1%85%B3%E1%86%AB%20%E1%84%80%E1%85%A5%E1%84%8B%E1%85%A3(Linger%20On)%20M-V.mp4?alt=media&token=05727f59-22c4-461f-bd62-4d95fc0c0a51"
+
+    private let videoPlayView: UIView = {
+        let videoView = UIView()
+        videoView.backgroundColor = .black
+
+        return videoView
+    }()
 
     private let cancelButton: UIButton = {
         let button = UIButton()
@@ -17,11 +27,21 @@ class VideoViewController: UIViewController{
         button.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
         return button
     }()
+
     private var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+
     @objc
     func cancelAction() {
         self.dismiss(animated: true, completion: nil)
     }
+
+    private let activityIndicator: UIActivityIndicatorView = {
+        let activity = UIActivityIndicatorView()
+        activity.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activity.hidesWhenStopped = false
+        activity.startAnimating()
+        return activity
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,6 +79,52 @@ class VideoViewController: UIViewController{
             $0.centerX.equalToSuperview()
             $0.height.equalTo(50)
             $0.width.equalTo(50)
+        }
+
+        if let url = URL(string: urlString) {
+            let player = AVPlayer(url: url)
+
+            let playerLayer = AVPlayerLayer(player: player)
+            self.videoPlayView.layer.addSublayer(playerLayer)
+
+            self.view.addSubview(videoPlayView)
+            videoPlayView.snp.makeConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+                $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+                $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+                $0.width.equalTo(view.snp.width)
+                $0.height.equalTo(250)
+
+            }
+            self.view.addSubview(activityIndicator)
+            activityIndicator.snp.makeConstraints {
+                $0.centerX.equalTo(videoPlayView.snp.centerX)
+                $0.centerY.equalTo(videoPlayView.snp.centerY)
+                $0.height.equalTo(70)
+                $0.width.equalTo(70)
+            }
+            playerLayer.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 250)
+            player.play()
+            player.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status) , options: [.old, .new], context: nil)
+        }
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+
+            switch status {
+            case .readyToPlay:
+                activityIndicator.stopAnimating()
+                activityIndicator.isHidden = true
+            default:
+                break
+            }
         }
     }
 }
